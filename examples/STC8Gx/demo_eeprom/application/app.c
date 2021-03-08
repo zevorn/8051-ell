a5bcff0999ff6af647551fda7a0271a75de57642
zeweni <209152007@qq.com>
2021-03-08 22:50:17 +0800

    更新 EEPROM 关于STC8G 的例程

--- examples/STC8Gx/demo_eeprom/application/app.c
+++ examples/STC8Gx/demo_eeprom/application/app.c
@@ -21,8 +21,10 @@ typedef struct
 }   PRAM_Type;
 
 PRAM_Type  SystemParam_Write;
-
 PRAM_Type  SystemParam_Read;
+
+static void Read_Flash(PRAM_Type *pRAM);
+static void Write_Flash(PRAM_Type *pRAM);
 /*-----------------------------------------------------------------------
 |                               FUNCTION                                |
 -----------------------------------------------------------------------*/
@@ -35,24 +37,43 @@ PRAM_Type  SystemParam_Read;
 ***/
 void eeprom_entry(void)
 {
-  EEPROM_Erase_Page(0x0010);
-  SystemParam_Write.LastInputChannel = 'A';
-  SystemParam_Write.motor_position = 'B';
+	EEPROM_Erase_Page(0x0010);
+	EEPROM_Erase_Page(0x0011);
+	
+	SystemParam_Write.LastInputChannel = 'A';
+	SystemParam_Write.motor_position = 'B';
   
-  EEPROM_Write_Byte(0x0010,SystemParam_Write.LastInputChannel);
-  EEPROM_Write_Byte(0x0011,SystemParam_Write.motor_position);
+	Write_Flash(&SystemParam_Write);
+	Read_Flash(&SystemParam_Read);
 	
-  SystemParam_Read.LastInputChannel = EEPROM_Read_Byte(0x0010);
-  SystemParam_Read.motor_position = EEPROM_Read_Byte(0x0011);	
-	
-  printf("%c \r\n",SystemParam_Read.LastInputChannel);
-  printf("%c \r\n",SystemParam_Read.motor_position);
+	printf("%c \r\n",SystemParam_Read.LastInputChannel);
+	printf("%c \r\n",SystemParam_Read.motor_position);
 	
 }
-	
 
-	
-	
+/**
+  * @name    Read_Flash
+  * @brief   read flash of device function
+  * @param   *pRAM PRAM_Type
+  * @return  None
+***/
+static void Read_Flash(PRAM_Type *pRAM)
+{
+	pRAM -> LastInputChannel = EEPROM_Read_Byte(0x0010);
+	pRAM -> motor_position = EEPROM_Read_Byte(0x0011);		
+}
+
+/**
+  * @name    Write_Flash
+  * @brief   write flash of device function
+  * @param   *pRAM PRAM_Type
+  * @return  None
+***/
+static void Write_Flash(PRAM_Type *pRAM)
+{
+  EEPROM_Write_Byte(0x0010,pRAM -> LastInputChannel);
+  EEPROM_Write_Byte(0x0011,pRAM -> motor_position);
+}
 /*-----------------------------------------------------------------------
 |                   END OF FLIE.  (C) COPYRIGHT zeweni                  |
 -----------------------------------------------------------------------*/
