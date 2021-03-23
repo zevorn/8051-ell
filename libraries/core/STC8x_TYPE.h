2cc9639e5399ca3685cb60adafe91632f543241d
zeweni <209152007@qq.com>
2021-03-23 18:15:54 +0800

    兼容RTOS

--- libraries/core/STC8x_TYPE.h
+++ libraries/core/STC8x_TYPE.h
@@ -23,93 +23,58 @@
 /*--------------------------------------------------------
 | @Description: Exact width signed integer types         |
 --------------------------------------------------------*/
+						
+typedef signed char     int8;	    //  8 bits 
+typedef signed int      int16;	// 16 bits 
+typedef signed long     int32;	// 32 bits 
 
-#ifndef INT8_T
-    #define INT8_T
-    typedef    signed char     int8_t;	    //  8 bits 
-#endif
-
-#ifndef INT16_T
-    #define INT16_T
-    typedef    signed int      int16_t;	// 16 bits 
-#endif
-
-#ifndef INT32_T
-    typedef    signed long     int32_t;	// 32 bits 
-#endif
+typedef  int8    int8_t;	//  8 bits 
+typedef  int16  int16_t;	// 16 bits 
+typedef  int32  int32_t;	// 32 bits 
 
 /*--------------------------------------------------------
 | @Description: Volatile width signed integer types      |
 --------------------------------------------------------*/
 
-#ifndef VINT8_T
-    #define VINT8_T
-    typedef    volatile int8_t     vint8_t;	//  8 bits 
-#endif
+typedef volatile int8   vint8;	//  8 bits 
+typedef volatile int16  vint16;	// 16 bits 
+typedef volatile int32  vint32;	// 32 bits 
 
-#ifndef VINT16_T
-    #define VINT16_T
-    typedef    volatile int16_t    vint16_t;	// 16 bits 
-#endif
-
-#ifndef VINT32_T
-    #define VINT32_T
-    typedef    volatile int32_t    vint32_t;	// 32 bits 
-#endif
+typedef  vint8    vint8_t; //  8 bits 
+typedef  vint16  vint16_t; // 16 bits 
+typedef  vint32  vint32_t; // 32 bits 
 
 /*--------------------------------------------------------
 | @Description: Exact width unsigned integer types       |
 --------------------------------------------------------*/
 
-#ifndef UINT8_T
-    #define UINT8_T
-    typedef     unsigned char    uint8_t;	//  8 bits 
-#endif
+typedef unsigned char   uint8;	//  8 bits 
+typedef unsigned int    uint16;	// 16 bits 
+typedef unsigned long   uint32;	// 32 bits 
 
-#ifndef UINT16_T
-    #define UINT16_T
-    typedef     unsigned int     uint16_t;	// 16 bits 
-#endif
-
-#ifndef UINT32_T
-    #define UINT32_T
-    typedef     unsigned long    uint32_t;	// 32 bits 
-#endif
+typedef  uint8    uint8_t; //  8 bits 
+typedef  uint16  uint16_t; // 16 bits 
+typedef  uint32  uint32_t; // 32 bits 
 
 /*--------------------------------------------------------
 | @Description: Volatile width unsigned integer types    |
 --------------------------------------------------------*/
 
-#ifndef VUINT8_T
-    #define VUINT8_T
-    typedef volatile uint8_t    vuint8_t;	//  8 bits 
-#endif
+typedef volatile uint8  vuint8;	//  8 bits 
+typedef volatile uint16 vuint16;	// 16 bits 
+typedef volatile uint32 vuint32;	// 32 bits 
 
-#ifndef VUINT16_T
-    #define VUINT16_T
-    typedef volatile uint16_t   vuint16_t;	// 16 bits 
-#endif
-
-#ifndef VUINT32_T
-    #define VUINT32_T
-    typedef volatile uint32_t   vuint32_t;	// 32 bits 
-#endif
+typedef  vuint8    vuint8_t; //  8 bits 
+typedef  vuint16  vuint16_t; // 16 bits 
+typedef  vuint32  vuint32_t; // 32 bits 
 
 /*--------------------------------------------------------
 | @Description: IO definitions                           |
 --------------------------------------------------------*/
 
-#ifndef __I
-    #define     __I    volatile   // 'read only' permissions 
-#endif
-
-#ifndef __O
-    #define     __O    volatile   // 'write only'  permissions 
-#endif
-
-#ifndef __IO
-    #define    __IO    volatile   // 'read / write' permissions 
-#endif
+#define    __I    volatile   // 'read only' permissions 
+#define    __O    volatile   // 'write only'  permissions 
+#define    __IO   volatile   // 'read / write' permissions 
 
 #endif
 /*-----------------------------------------------------------------------
